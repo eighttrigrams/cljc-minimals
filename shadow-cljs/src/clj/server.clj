030e11e96d0c1899b98fed7eb149143caeb243ee
Dan <dan@eighttrigrams.net>
2022-06-01 20:46:36 +0100

    Improve compojure part a bit

--- shadow-cljs/src/clj/server.clj
+++ shadow-cljs/src/clj/server.clj
@@ -1,21 +1,24 @@
 (ns server
   (:require [ring.adapter.jetty :as j]
-            [compojure.core :as c]
-            [compojure.route :as route]
-            [ring.util.response :as resp]
-            [ring.middleware.json :as json]))
+            [compojure.core :refer [defroutes GET POST]]
+            [ring.util.response :as response]
+            [ring.middleware.json :as json]
+            [ring.middleware.resource :refer [wrap-resource]]))
 
 (defn api-handler [{{msg :msg} :body}]
   {:body {:echo msg}})
 
 (defn wrap-api [handler]
-  (-> handler 
-      json/wrap-json-response 
+  (-> handler
+      json/wrap-json-response
       (json/wrap-json-body {:keywords? true})))
 
-(c/defroutes app
-  (c/POST "/api" [] (wrap-api api-handler))
-  (c/GET "/" [] (resp/redirect "/index.html"))
-  (route/resources "/"))
+(defroutes routes
+  (POST "/api" [] (wrap-api api-handler))
+  (GET "/" [] (response/resource-response "public/index.html")))
+
+(def app
+  (-> routes
+      (wrap-resource "public")))
 
 (j/run-jetty app {:port 3000})
